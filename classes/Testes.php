cdb7f16ca0678e4e0491994ed5ca598943ba6e49
Ricardo CF <ri_ca_fa@hotmail.com>
2018-01-28 00:51:01 -0200

    improving test syntax

--- classes/Testes.php
+++ classes/Testes.php
@@ -1,13 +1,11 @@
 <?php
 require_once('../simpletest/autorun.php');
-require_once('log_test.php');
-require_once('calculadora_test.php');
 
 class AllTests extends TestSuite {
     function __construct() {
         parent::__construct();
-        //$this->add(new TestOfLogging());
-        //$this->add(new CalculadoraTest());
+        $this->addFile('calculadora_test.php');
+        $this->addFile('log_test.php');
     }
 }
 ?>
